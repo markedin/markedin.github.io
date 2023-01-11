121ab2da41f6251e90ab74a85466f61636f7d112
markedin <119455170+markedin@users.noreply.github.com>
2023-01-11 14:54:03 -0800

    This push includes the updated version of my portfolio website
    added HTML/CSS/JS to update my personal portfolio.

---
+++ JS/main.js
@@ -0,0 +1,49 @@
+//These functions open and close contact form
+function openForm() {
+    document.getElementById("myForm").style.display = "block";
+}
+
+function closeForm() {
+    document.getElementById("myForm").style.display = "none";
+}
+
+//this function displays first image in slideshow when page loads
+var slideIndex = 1;
+showSlides(slideIndex);
+
+//this function changes the slide when the left or right arrows are clicked
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+//this function changes the slide when the dots are clicked
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot"); 
+    
+    if (n > slides.length) {slideIndex = 1};
+    if (n < 1) {slideIndex = slides.length}; 
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none"; 
+    }
+
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", ""); 
+    }
+
+    slides[slideIndex - 1].style.display = "block"; 
+    dots[slideIndex - 1].className += " active";
+}
+
+//this code will create close the contact form when user clicks off it
+//first step is to add event listener for any clicks on the website
+document.addEventListener("click", function(event){
+    if (event.target.matches(".cancel") || !event.target.closest(".form-popup") && !event.target.closest(".Pop_Up_Button") && !event.target.closest(".contact")){
+        closeForm()
+    }
+
+}, false )
